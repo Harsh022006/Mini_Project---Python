cfd1933c84d284932d8e9fe205ebea08512ba541
Harsh022006 <harshamburkar@gmail.com>
2025-11-07 15:20:29 +0530

    practice

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('contact',views.contact,name='contact'),
     path('login',views.login,name='login'),
     path('signup',views.signup,name='signup'),
+    path('logout',views.logout,name='logout'),
 ]
